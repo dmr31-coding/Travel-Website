a34964b433fbb306a892837a6bd926bf105cb791
Dinesh Magar <magard1010@gmail.com>
2024-08-08 13:31:57 +0545

    travelwevsite nav scss components

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+// import { useState } from 'react'
+
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+      
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+import React from 'react'
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Main from './Components/Main/Main'
+import Footer from './Components/Footer/Footer'
+
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    {/* <Home/>
+    <Main/>
+    <Footer/> */}
+    </>
+  )
+}
+
+export default App
