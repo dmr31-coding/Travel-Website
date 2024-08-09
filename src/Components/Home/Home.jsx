6e3a4e86d4d0533daca933e48159888f0c790b4f
Dinesh Magar <magard1010@gmail.com>
2024-08-09 23:07:24 +0545

    Card destinationSearch HomeContent

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import './home.css'
+import './home.scss'
+import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
 
 import video from '../../assets/video.mp4'
 
@@ -8,6 +10,59 @@ const Home = () => {
     <section className="home">
       <div className="overlay"></div>
       <video src={video} muted autoPlay loop typeof='video/mp4'></video>
+
+      <div className="homeContent container">
+        <div className="textDiv">
+
+          <span className="smallText">
+            Our Packages
+          </span>
+
+          <h1 className='homeTitle'>
+            Search Your Holiday
+          </h1>
+        </div>
+
+        <div className="cardDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">Search your destination:</label>
+            <div className="input flex">
+              <input type="text" placeholder='Enter name here...' />
+              <GrLocation className='icon' />
+            </div>
+          </div>
+
+          
+
+          <div className="dateInput">
+            <label htmlFor="date">Select your date:</label>
+            <div className="input flex">
+              <input type="date" />
+              
+            </div>
+          </div>
+
+          <div className="priceInput">
+            <div className="label_total flex">
+              <label htmlFor="price">Max price:</label>
+              <h3 className='total'>$5000</h3>
+            </div>
+            
+            <div className="input flex">
+              <input type="range" max="5000" min="1000" />
+            </div>
+          </div>
+
+          <div className="searchOptions flex">
+            <HiFilter className='icon' />
+            <span>MORE FILTERS</span>
+          </div>
+
+
+        </div>
+
+        
+      </div>
     </section>
   )
 }
